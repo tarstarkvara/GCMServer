de46bc301580d22a36bb1558e8cac513424a62aa
Sander <tarssander1@gmail.com>
2015-12-02 15:00:20 +0200

    testing upstream channel

--- src/main/java/mcm/notification/services/gcm/SendAllMessagesServlet.java
+++ src/main/java/mcm/notification/services/gcm/SendAllMessagesServlet.java
@@ -126,6 +126,7 @@ public class SendAllMessagesServlet extends BaseServlet {
       for (int i = 0; i < results.size(); i++) {
         Result result = results.get(i);
         if (result.getMessageId() != null) {
+		  status.append(devices.get(i));
           status.append("Succesfully sent message to device #").append(i);
           String canonicalRegId = result.getCanonicalRegistrationId();
           if (canonicalRegId != null) {
